c42c52dacefcebef93274db6895bacc37c819acb
holyrvbible <76605844+holyrvbible@users.noreply.github.com>
2020-12-25 20:48:57 -0800

    Delete mac-chrome-webapp.sh

--- mac-chrome-webapp.sh
+++
@@ -1,2 +0,0 @@
-# Mac: Open bible as a Chrome frameless window using online hosted files.
-"/Applications/Google Chrome.app/Contents/MacOS/Google Chrome" --app=https://holyrcvbible.000webhostapp.com/index.html
